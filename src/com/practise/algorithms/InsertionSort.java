1d638245419817862b4d08090bc6227e5c4590a6
Deepa Sunkara <suresh.malamanti@gmail.com>
2018-06-15 22:42:13 -0700

    new method added for decreasing order

--- src/com/practise/algorithms/InsertionSort.java
+++ src/com/practise/algorithms/InsertionSort.java
@@ -11,33 +11,58 @@ public class InsertionSort
 {
 
 	
-	public static int[] sort(int[] array)
+	/** sorting in increasing order*/
+	
+	public static int[] sort(int[] numbers)
 	{
 		int j=0;
 		
-		for(int i=1;i<array.length;i++)
+		for(int i=1;i<numbers.length;i++)
 		{
-			int tmp = array[i];
+			int tmp = numbers[i];
 			
 			j=i-1;
 			
-			while(j >= 0 && array[j] > tmp)
+			while(j >= 0 && numbers[j] > tmp)
 			{
-				array[j+1] = array[j];
+				numbers[j+1] = numbers[j];
 				j = j-1;
 			}
 			
-			array[j+1] = tmp; 
+			numbers[j+1] = tmp; 
 			
 		}
 		
-		return array;
+		return numbers;
+	}
+	
+	public static int[] sortDecreasingOrder(int[] numbers)
+	{
+		int j=0;
+		
+		for(int i=1;i<numbers.length;i++)
+		{
+			int tmp = numbers[i];
+			
+			j=i-1;
+			
+			while(j >= 0 && numbers[j] < tmp)
+			{
+				numbers[j+1] = numbers[j];
+				j = j-1;
+			}
+			
+			numbers[j+1] = tmp; 
+			
+		}
+		
+		return numbers;
 	}
 	public static void main(String[] args)
 	{
 		int[] numbers = {5,2,4,6,1,3};
 		
-		numbers = sort(numbers);
+		numbers = sortDecreasingOrder(numbers);
 		
 		for(int i=0;i<numbers.length;i++)
 		{
